f081101ffa045f44aa2c9167e44fa5eeef3b4cdc
Roberto <roberto.p.gomes@hotmail.com>
2022-05-12 23:23:52 -0300

    Fix script

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,12 +1,12 @@
-import { Express, Router } from 'express'
-import { readdirSync } from 'fs'
+import { Express, Router } from "express";
+import { readdirSync } from "fs";
 
 export default (app: Express): void => {
-  const router = Router()
-  app.use('/api', router)
+  const router = Router();
+  app.use("/api", router);
   // eslint-disable-next-line node/no-path-concat
   readdirSync(`${__dirname}/../routes`).map(async (fileName) => {
     // eslint-disable-next-line prettier/prettier
-    (await import(`../routes/${fileName}`)).default(router)
-  })
-}
+    (await import(`../routes/${fileName}`)).default(router);
+  });
+};
